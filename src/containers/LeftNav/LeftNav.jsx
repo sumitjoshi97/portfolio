950ff3f4dc728a8f3437bd022c0987fcb7c31d85
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-06-03 19:27:09 +0530

    updated styles for left navbar

--- src/containers/LeftNav/LeftNav.jsx
+++ src/containers/LeftNav/LeftNav.jsx
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
-
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import React, {Component} from 'react';
+import NavLink from './NavLink/NavLink';
 import './LeftNav.css';
 
-
 class LeftNav extends Component {
-    state = {
-
-    }
+    state = {}
 
     render() {
         return (
             <div className="left-nav">
                 <div className="left-nav__profile"></div>
 
-                <ul className="left-nav__link-box">
-                    <li className="left-nav__link-box--item"><AnchorLink className="left-nav__link-box--item-link" href="#home"><span></span></AnchorLink></li>
-                    <li className="left-nav__link-box--item"><AnchorLink className="left-nav__link-box--item-link" href="#about"><span></span></AnchorLink></li>
-                    <li className="left-nav__link-box--item"><AnchorLink className="left-nav__link-box--item-link" href="#skills"><span></span></AnchorLink></li>
-                    <li className="left-nav__link-box--item"><AnchorLink className="left-nav__link-box--item-link" href="#projects"><span></span></AnchorLink></li>
+                <ul className="left-nav__link-box" id="fixed-nav">
+                    <NavLink link="home" anchor="home"/>
+                    <NavLink link="about" anchor="about"/>
+                    <NavLink link="skills" anchor="skills"/>
+                    <NavLink link="projects" anchor="projects"/>
                 </ul>
             </div>
         )
