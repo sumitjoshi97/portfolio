efa556b965f887bd1f0d2d2debf171f4194a3b71
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-06-03 13:08:46 +0530

    added fullpage to main

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,44 +1,58 @@
 import React, {Component} from 'react';
 
+import $ from 'jquery';
+import 'fullpage.js';
+
 import './Main.css';
 
 class Main extends Component {
-    state = {};
+
+    componentDidMount() {
+        $(document)
+            .ready(function () {
+                $("#fullpage").fullpage({
+                    anchors: ["home", "about", "skills", "projects"]
+                });
+            });
+    }
 
     render() {
         return (
-            <div className="main">
-                <div className="main__home">
+            <div className="main" id="fullpage">
+                <div className="main__home section">
                     <h1 className="heading-primary">Sumit Joshi</h1>
                 </div>
 
-                <div className="main__about">
+                <div className="main__about section">
                     <p className="main__about--text">
-                        Hi, I am Sumit Joshi. I am a front-end developer/designer based on Delhi, India.
-                        I have a passion for web design and love to develop web applications, websites
-                        for desktop and mobile devices. I fond of React js
+                        Hi, I am
+                        <span className="green">Sumit Joshi</span>. I am a
+                        <span className="red">front-end developer/designer</span>
+                        based on Delhi, India. I have a passion for web design and love to develop web
+                        applications, websites for desktop and mobile devices. I fond of
+                        <span className="red">React js</span>
                     </p>
                 </div>
 
-                <div className="main__skills">
+                <div className="main__skills section">
                     <h2 className="heading-secondary">Skills</h2>
                     <ul className="skills-list">
-                        <li className="skills-list-item">HTML and CSS</li>
-                        <li className="skills-list-item">Javascript</li>
-                        <li className="skills-list-item">Sass</li>
-                        <li className="skills-list-item">React js</li>
-                        <li className="skills-list-item">Redux</li>
-                        <li className="skills-list-item">Mongo DB</li>
-                        <li className="skills-list-item">Express js</li>
-                        <li className="skills-list-item">Node js</li>
-                        <li className="skills-list-item">Jest</li>
-                        <li className="skills-list-item">Adobe XD</li>
-                        <li className="skills-list-item">Adobe Photoshop</li>
-                        <li className="skills-list-item">Adobe Illustrator</li>
+                        <li className="skills-list__item">HTML and CSS</li>
+                        <li className="skills-list__item">Javascript</li>
+                        <li className="skills-list__item">Sass</li>
+                        <li className="skills-list__item">React js</li>
+                        <li className="skills-list__item">Redux</li>
+                        <li className="skills-list__item">Mongo DB</li>
+                        <li className="skills-list__item">Express js</li>
+                        <li className="skills-list__item">Node js</li>
+                        <li className="skills-list__item">Jest</li>
+                        <li className="skills-list__item">Adobe XD</li>
+                        <li className="skills-list__item">Adobe Photoshop</li>
+                        <li className="skills-list__item">Adobe Illustrator</li>
                     </ul>
                 </div>
 
-                <div className="main__projects">
+                <div className="main__projects section">
                     <div className="main__projects--project1">
                         <div className="project-text">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, libero offic {/* anchr */}
@@ -52,7 +66,7 @@ class Main extends Component {
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
