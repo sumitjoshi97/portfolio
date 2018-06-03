5da71be9b6d139e4d5ccf6f553ac40d984b70814
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-06-03 19:26:37 +0530

    added smooth scrolling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
-
-// import LeftNav from './containers/LeftNav/LeftNav';
-// import RightNav from './containers/RightNav/RightNav';
+import React, {Component} from 'react';
+import $ from 'jquery';
+import 'fullpage.js';
+import LeftNav from './containers/LeftNav/LeftNav';
+import RightNav from './containers/RightNav/RightNav';
 import Main from './containers/Main/Main';
-
+import ReallySmoothScroll from 'really-smooth-scroll';
 import './App.css';
 
+ReallySmoothScroll.shim();
 
 class App extends Component {
+  componentDidMount() {
+    $(document)
+        .ready(function () {
+            $("#fullpage").fullpage({
+                anchors: ["home", "about", "skills", "projects"],
+                menu: "#fixed-nav"
+            });
+        });
+}
+
   render() {
     return (
       <div className="welcome">
-        {/* <LeftNav/> */}
+        <LeftNav/>
         <Main/>
-        {/* <RightNav/> */}
+        <RightNav/>
       </div>
     );
   }
