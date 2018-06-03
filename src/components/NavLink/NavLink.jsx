11520eaa2b09424da625b036022fed1d7d85e511
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-06-03 19:29:49 +0530

    added navlink component

---
+++ src/components/NavLink/NavLink.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NavLink.css';
+
+const NavLink = props => (
+    <li className="left-nav__link-box--item" data-menuanchor={props.anchor}>
+        <a
+            className="left-nav__link-box--item-link"
+            href={`#${props.link}`}>
+            <span className="active"></span>
+        </a>
+    </li>
+)
+
+export default NavLink;
