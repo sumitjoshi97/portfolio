f973ab909fe6d5301d0f4403475f6bdea0e6ff90
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-06-06 14:55:21 +0530

    right nav component refactored and changed

--- src/containers/RightNav/RightNav.jsx
+++ src/containers/RightNav/RightNav.jsx
@@ -1,20 +1,31 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import Icons from '../../assets/svg/sprite.svg';
+// import NavToggle from './NavToggle/NavToggle';
 import './RightNav.css';
 
 class RightNav extends Component {
-    state = {
-
-    }
-
     render() {
         return (
             <div className="right-nav">
-                <div className="nav-toggle"></div>
+                {/* <NavToggle /> */}
+
                 <div className="social">
-                    <div className="social--item"></div>
-                    <div className="social--item"></div>
-                    <div className="social--item"></div>
+                    <a href="mailto:dev.sumitjoshi@gmail.com" className="social--item">
+                        <svg className="social__icon">
+                            <use xlinkHref={`${Icons}#icon-mail`}></use>
+                        </svg>
+                    </a>
+
+                    <a href="https://github.com/sumitjoshi97" target="blank" className="social--item">
+                        <svg className="social__icon">
+                            <use xlinkHref={`${Icons}#icon-github`}></use>
+                        </svg>
+                    </a>
+                    <a href="https://linkedin.com/sumitjoshi" target="blank" className="social--item">
+                        <svg className="social__icon">
+                            <use xlinkHref={`${Icons}#icon-linkedin`}></use>
+                        </svg>
+                    </a>
                 </div>
             </div>
         )
