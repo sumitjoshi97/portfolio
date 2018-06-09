b684ba3ddfdf57e5ff0f21acda97eb866216c6cb
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-06-09 23:02:28 +0530

    home component added

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,77 @@
+import React, {Component} from 'react'
+import $ from 'jquery';
+import 'fullpage.js';
+import LeftNav from '../LeftNav/LeftNav';
+import RightNav from '../RightNav/RightNav';
+import Main from '../Main/Main';
+import NavToggle from '../NavToggle/NavToggle';
+import Navigation from '../Navigation/Navigation';
+import './Home.css';
+
+export default class Home extends Component {
+    state = {
+        isNavActive: false
+    }
+    componentDidMount() {
+        $(document)
+            .ready(function () {
+                $("#fullpage").fullpage({
+                    anchors: [
+                        "home", "about", "skills", "projects"
+                    ],
+                    menu: "#fixed-nav"
+                });
+            });
+        console.log('did mount')
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextState.isNavActive!==this.state.isNavActive
+    }
+    componentDidUpdate() {
+        if (typeof $.fn.fullpage !== 'undefined') {     
+            $.fn.fullpage.destroy('all'); 
+        }
+        $("#fullpage").fullpage({
+            anchors: [
+                "home", "about", "skills", "projects"
+            ],
+            menu: "#fixed-nav"
+        });
+        console.log('update');
+    }
+
+    componentWillUnmount() {
+        if (typeof $.fn.fullpage !== 'undefined') {     
+            $.fn.fullpage.destroy('all'); 
+        }
+    }
+
+    handleToggle = () => {
+        this.setState({
+            isNavActive: !this.state.isNavActive
+        });
+    }
+
+    renderNav = () => {
+        if (this.state.isNavActive) {
+            return <Navigation toggle={this.handleToggle} isNavActive={this.state.isNavActive}/>
+        }
+        return (
+            <div>
+                <LeftNav/>
+                <Main/>
+                <RightNav/>
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            <div className="home"> 
+                <NavToggle toggle={this.handleToggle}/>
+                {this.renderNav()}
+            </div>
+        )
+    }
+}
