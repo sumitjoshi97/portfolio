497327753c2b71b53d9473ad0b906642de911c37
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-06-09 23:02:02 +0530

    app component refactored

--- src/App.js
+++ src/App.js
@@ -1,78 +1,23 @@
 import React, {Component} from 'react';
-import $ from 'jquery';
-import 'fullpage.js';
-import LeftNav from './containers/LeftNav/LeftNav';
-import RightNav from './containers/RightNav/RightNav';
-import Main from './containers/Main/Main';
-import NavToggle from './containers/NavToggle/NavToggle';
-import Navigation from './containers/Navigation/Navigation';
+import Home from './containers/Home/Home';
+import Projects from './containers/Projects/Projects';
 import ReallySmoothScroll from 'really-smooth-scroll';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 ReallySmoothScroll.shim();
 
 class App extends Component {
-    state = {
-        isNavActive: false
-    }
-    componentDidMount() {
-        $(document)
-            .ready(function () {
-                // if (typeof $.fn.fullpage !== 'undefined') {     $.fn.fullpage
-                // .destroy('all'); }
-                $("#fullpage").fullpage({
-                    anchors: [
-                        "home", "about", "skills", "projects"
-                    ],
-                    menu: "#fixed-nav"
-                });
-            });
-
-        console.log('did mount')
-    }
-    componentDidUpdate() {
-
-        if (typeof $.fn.fullpage !== 'undefined') {     
-            $.fn.fullpage.destroy('all'); 
-        }
-        $("#fullpage").fullpage({
-            anchors: [
-                "home", "about", "skills", "projects"
-            ],
-            menu: "#fixed-nav"
-        });
-
-    }
-
-    componentWillUnmount() {
-        console.log('unmount')
-    }
-
-    handleToggle = () => {
-        this.setState({
-            isNavActive: !this.state.isNavActive
-        })
-    }
-
-    renderNav = () => {
-        if (this.state.isNavActive) {
-            return <Navigation toggle={this.handleToggle} isNavActive={this.state.isNavActive}/>
-        }
-        return (
-            <div>
-                <LeftNav/>
-                <Main/>
-                <RightNav/>
-            </div>
-
-        )
-    }
-
     render() {
         return (
-            <div className="welcome">
-                <NavToggle toggle={this.handleToggle}/> {this.renderNav()}
-            </div>
+            <Router>
+                <div className="welcome">
+                    <Switch>
+                        <Route exact path="/projects-all" component={Projects}/>
+                        <Route path="/" component={Home}/>
+                    </Switch>
+                </div>
+            </Router>
         );
     }
 }
