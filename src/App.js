ac41d42cb288c63920367088a06d00c3ce871e71
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-06-14 18:40:19 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import Portfolio from './containers/Portfolio/PortFolio';
 import ProjectsList from './containers/ProjectsList/ProjectsList';
 import ReallySmoothScroll from 'really-smooth-scroll';
-import './App.css';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import ProjectEmaily from './containers/ProjectsList/ProjectEmaily/ProjectEmaily';
 import ProjectNatours from './containers/ProjectsList/ProjectNatours/ProjectNatours';
@@ -12,14 +11,12 @@ class App extends Component {
     render() {
         return (
             <Router>
-                <div className="welcome">
-                    <Switch>
-                        <Route exact path="/projects" component={ProjectsList}/>
-                        <Route exact path="/projects/emaily" component={ProjectEmaily}/>
-                        <Route exact path="/projects/natours" component={ProjectNatours}/>
-                        <Route path="/" component={Portfolio}/>
-                    </Switch>
-                </div>
+                <Switch>
+                    <Route exact path="/projects" component={ProjectsList}/>
+                    <Route exact path="/projects/emaily" component={ProjectEmaily}/>
+                    <Route exact path="/projects/natours" component={ProjectNatours}/>
+                    <Route path="/" component={Portfolio}/>
+                </Switch>
             </Router>
         );
     }
