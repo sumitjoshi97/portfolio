916cd1309f165703cf278b7e3269dd634d5b0113
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-07-13 16:12:19 +0530

    media queres added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'fullpage.js/dist/jquery.fullpage.min.css';
+import 'fullpage.js/dist/jquery.fullpage.min.js';
+
 import registerServiceWorker from './registerServiceWorker';
 // import Home from './containers/Home/Home';
 
