d2a4138c30b1b7b16910f02dca7d2c21ec518184
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-07-24 11:04:34 +0530

    new routes added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import ScrollToTop from './ScrollToTop';
 import ProjectEmaily from './containers/ProjectsList/ProjectEmaily/ProjectEmaily';
 import ProjectNatours from './containers/ProjectsList/ProjectNatours/ProjectNatours';
+import ProjectSmartBrain from './containers/ProjectsList/ProjectSmartBrain/ProjectSmartBrain';
+import ProjectBurger from './containers/ProjectsList/ProjectBurger/ProjectBurger';
+
 ReallySmoothScroll.shim();
 
 class App extends Component {
@@ -17,6 +20,8 @@ class App extends Component {
                         <Route exact path="/projects" component={ProjectsList}/>
                         <Route exact path="/projects/emaily" component={ProjectEmaily}/>
                         <Route exact path="/projects/natours" component={ProjectNatours}/>
+                        <Route exact path="/projects/smart-brain" component={ProjectSmartBrain}/>
+                        <Route path='/projects/burger' component={ProjectBurger}/>
                         <Route path="/" component={Portfolio}/>
                     </Switch>
                 </ScrollToTop>
