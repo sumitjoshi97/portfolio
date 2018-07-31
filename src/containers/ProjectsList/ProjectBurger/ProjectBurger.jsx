13f2bc2eeb0c20220572ba72836a60e8ec65ecd2
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-07-31 22:49:28 +0530

    burger component added

---
+++ src/containers/ProjectsList/ProjectBurger/ProjectBurger.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import Project from "../Project/Project";
+
+class ProjectBurger extends Component {
+    componentDidMount() {
+        window.moveTo(0,0);
+    }
+    render() {
+        return (
+            <Project 
+                projectType="App"
+                projectName="Burger Builder"
+                projectInfo=""
+                headerImage={require('../../../assets/images/burger/initial.jpg')}
+                headerStyle={['#d35400', '#e67e22']}
+                theme={'#f0932b'}
+                stacks = {['React js', 'Redux', 'Firebase']}
+                backImage={require('../../../assets/images/burger/screenshot.png')}
+                linkLanuchProject="https://bit.ly/_Burger"
+                screenshots = {[`${require('../../../assets/images/burger/initial.jpg')}`,
+                    `${require('../../../assets/images/burger/login.jpg')}`,
+                    `${require('../../../assets/images/burger/order.jpg')}`,
+                    `${require('../../../assets/images/burger/modal.jpg')}`,
+                    `${require('../../../assets/images/burger/orderData.jpg')}`,
+                    `${require('../../../assets/images/burger/ordersList.jpg')}`
+                    ]}
+                projectPrev='smart-brain'
+                projectNext='emaily'
+                />
+        )
+    }
+}
+
+export default ProjectBurger;
