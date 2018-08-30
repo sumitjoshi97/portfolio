00bbe4b6488495a025d5dd3e5adb982e1c46bbf1
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-30 15:36:07 +0530

    lazy loading added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
-import Portfolio from './containers/Portfolio/PortFolio';
-import ProjectsList from './containers/ProjectsList/ProjectsList';
-import ReallySmoothScroll from 'really-smooth-scroll';
-import { Route, Switch } from "react-router-dom";
+import React, {Component} from 'react';
+import asyncComponent from './hoc/AsyncComponent/AsyncComponent'
+import {Route, Switch} from "react-router-dom";
 import ScrollToTop from './ScrollToTop';
-import ProjectEmaily from './containers/ProjectsList/ProjectEmaily/ProjectEmaily';
-import ProjectNatours from './containers/ProjectsList/ProjectNatours/ProjectNatours';
-import ProjectSmartBrain from './containers/ProjectsList/ProjectSmartBrain/ProjectSmartBrain';
-import ProjectBurger from './containers/ProjectsList/ProjectBurger/ProjectBurger';
+import ReallySmoothScroll from 'really-smooth-scroll';
 ReallySmoothScroll.shim();
+
+const Portfolio = asyncComponent(() => import ('./containers/Portfolio/PortFolio').then(module => module.default))
+const ProjectsList = asyncComponent(() => import ('./containers/ProjectsList/ProjectsList').then(module => module.default))
+const ProjectEmaily = asyncComponent(() => import ('./containers/ProjectsList/ProjectEmaily/ProjectEmaily').then(module => module.default))
+const ProjectNatours = asyncComponent(() => import ('./containers/ProjectsList/ProjectNatours/ProjectNatours').then(module => module.default))
+const ProjectSmartBrain = asyncComponent(() => import ('./containers/ProjectsList/ProjectSmartBrain/ProjectSmartBrain').then(module => module.default))
+const ProjectBurger = asyncComponent(() => import ('./containers/ProjectsList/ProjectBurger/ProjectBurger').then(module => module.default))
 
 class App extends Component {
 
     render() {
+        
         return (
             <ScrollToTop>
                 <Switch>
-                    <Route exact path="/projects" component={ProjectsList} />
-                    <Route exact path="/projects/emaily" component={ProjectEmaily} />
-                    <Route exact path="/projects/natours" component={ProjectNatours} />
-                    <Route exact path="/projects/smart-brain" component={ProjectSmartBrain} />
-                    <Route path='/projects/burger' component={ProjectBurger} />
-                    <Route path="/" component={Portfolio} />
+                    <Route exact path="/projects" component={ProjectsList}/>
+                    <Route exact path="/projects/emaily" component={ProjectEmaily}/>
+                    <Route exact path="/projects/natours" component={ProjectNatours}/>
+                    <Route exact path="/projects/smart-brain" component={ProjectSmartBrain}/>
+                    <Route path='/projects/burger' component={ProjectBurger}/>
+                    <Route path="/" component={Portfolio}/>
                 </Switch>
             </ScrollToTop>
         );
