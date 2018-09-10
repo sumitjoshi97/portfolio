79bc08e441e75c0cfb45cdbd14cf6e6573aaa533
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-10 16:08:31 +0530

    smooth scroll in component

--- src/containers/ProjectsList/Project/Project.jsx
+++ src/containers/ProjectsList/Project/Project.jsx
@@ -5,6 +5,8 @@ import Header from './Header.jsx';
 import Footer from './Footer';
 import Radium from 'radium';
 import './Project.css';
+import ReallySmoothScroll from 'really-smooth-scroll';
+ReallySmoothScroll.shim();
 
 const Project = (props) => {
     
