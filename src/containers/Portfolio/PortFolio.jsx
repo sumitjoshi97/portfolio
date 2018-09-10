3f0dec1344262ef4fdb517e09a3264e0a117707e
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-10 16:06:17 +0530

    reafactored

--- src/containers/Portfolio/PortFolio.jsx
+++ src/containers/Portfolio/PortFolio.jsx
@@ -1,6 +1,4 @@
-import React, {Component} from 'react'
-import $ from 'jquery';
-import 'fullpage.js';
+import React, { Component } from 'react'
 import LeftNav from './LeftNav/LeftNav';
 import RightNav from './RightNav/RightNav';
 import Home from './Home/Home';
@@ -8,44 +6,15 @@ import NavToggle from './NavToggle/NavToggle';
 import Navigation from './Navigation/Navigation';
 import './PortFolio.css';
 
+const fullpageOptions = {
+    anchors: ["home", "about", "skills", "recents"],
+    menu: "#fixed-nav",
+    css3: 'true'
+}
+
 class Portfolio extends Component {
     state = {
         isNavActive: false
-    }
-    
-    componentDidMount() {
-        $(document)
-            .ready(function () {
-                $("#fullpage").fullpage({
-                    anchors: [
-                        "home", "about", "skills", "recents"
-                    ],
-                    menu: "#fixed-nav",
-                    css3: 'true'
-                });
-            });
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextState.isNavActive!==this.state.isNavActive
-    }
-
-    componentDidUpdate() {
-        if (typeof $.fn.fullpage !== 'undefined') {     
-            $.fn.fullpage.destroy('all'); 
-        }
-        $("#fullpage").fullpage({
-            anchors: [
-                "home", "about", "skills", "recents"
-            ],
-            menu: "#fixed-nav"
-        });
-    }
-
-    componentWillUnmount() {
-        if (typeof $.fn.fullpage !== 'undefined') {     
-            $.fn.fullpage.destroy('all'); 
-        }
     }
 
     handleToggle = () => {
@@ -56,21 +25,21 @@ class Portfolio extends Component {
 
     renderNav = () => {
         if (this.state.isNavActive) {
-            return <Navigation toggle={this.handleToggle} isNavActive={this.state.isNavActive}/>
+            return <Navigation toggle={this.handleToggle} isNavActive={this.state.isNavActive} />
         }
         return (
             <div className="portfolio">
-                <LeftNav/>
-                <Home/>
-                <RightNav/>
+                <LeftNav />
+                <Home {...fullpageOptions} />
+                <RightNav />
             </div>
         )
     }
 
     render() {
         return (
-            <div className="home "> 
-                <NavToggle toggle={this.handleToggle}/>
+            <div className="home ">
+                <NavToggle toggle={this.handleToggle} />
                 {this.renderNav()}
             </div>
         )
