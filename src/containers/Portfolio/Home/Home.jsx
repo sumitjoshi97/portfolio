caf1c15b572738d68e0fece26cbec2369d5df0fe
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-10 16:03:37 +0530

    fullpage-react package used instead of fullpage js

--- src/containers/Portfolio/Home/Home.jsx
+++ src/containers/Portfolio/Home/Home.jsx
@@ -1,104 +1,112 @@
-import React, {Component} from 'react';
-import Icons from '../../../assets/svg/sprite.svg';
-import {Link} from 'react-router-dom';
+import React from 'react'
+import Icons from '../../../assets/svg/sprite.svg'
+import {Link} from 'react-router-dom'
+import ReactFullpage from '@fullpage/react-fullpage'
 import './Home.css';
-import './HomeAnimate.css';
+import './HomeAnimate.css'
+import HeroBackground from './HeroBackground/HeroBackground'
 
-import HeroBackground from './HeroBackground/HeroBackground';
+const Home = fullpageProps => (
+    <ReactFullpage 
+        {...fullpageProps} 
+        className="main"
+        render={() => {
+            return (
+                <div className="main">
 
-
-class Home extends Component {
-    render() {
-        return (
-            <div className="main" id="fullpage">
-                <div className="main__home section">
-                    <HeroBackground/>
-                    <h1 className="heading-primary"><span>Sumit</span> Joshi</h1>
-                </div>
-
-                <div className="main__about section">
-                    <div className="bar-left"></div>
-
-                    <p className="main__about--text">
-                        <span className="main__about--text--header red">Hi!</span>, I am
-                        <span className="green">&nbsp;Sumit Joshi</span>. I'm a
-                        <span className="red">&nbsp;front-end software developer&nbsp;</span>
-                        based in Delhi, India. I am comfortable throughout the stack, but my passion lies in front-end, at the intersection of code and design.
-                        <br/>
-                        Outside of code, I like to <span className="red">&nbsp;draw&nbsp;</span>, play 
-                        <span className="red">&nbsp;video games&nbsp;</span> and convert my ideas to video games.
-                    </p>
-
-                </div>
-
-                <div className="main__skills section">
-                    <div className="bar-left"></div>
-                    
-                    <h2 className="main__skills__heading">skills</h2>
-
-                    <ul className="skills-list">
-                        <li className="skills-list__item skills-list__heading green">front</li>
-                        <li className="skills-list__item">html</li>
-                        <li className="skills-list__item">css</li>
-                        <li className="skills-list__item">sass</li>
-                        <li className="skills-list__item">javascript es6</li>
-                        <li className="skills-list__item">jQuery</li>
-                        <li className="skills-list__item">react.js</li>
-                        <li className="skills-list__item">redux</li>
-                        <li className="skills-list__item skills-list__heading red">back</li>
-                        <li className="skills-list__item">node.js</li>
-                        <li className="skills-list__item">express.js</li>
-                        <li className="skills-list__item">npm</li>
-                        <li className="skills-list__item">mongoDB</li>
-                        <li className="skills-list__item">mysql</li>
-                        <li className="skills-list__item">postresql</li>
-                        <li className="skills-list__item">rest</li>
-                        <li className="skills-list__item skills-list__heading green">tools</li>
-                        <li className="skills-list__item">git</li>
-                        <li className="skills-list__item">adobe XD</li>
-                        <li className="skills-list__item">adobe photoshop</li>
-                        <li className="skills-list__item">adobe illustrator</li>
-                        <li className="skills-list__item">visual code</li>
-                    </ul>
-                </div>
-
-                <div className="main__projects section">
-                    <div className="bar-left"></div>
-                    
-                    <div className="main__projects__header">
-                        <h3 className="recents-heading">
-                            <span>Recent</span>
-                            <span>Projects</span>
-                        </h3>
-                        <Link to="/projects" className="main__projects__header--link">
-                            See all projects
-                            <svg className="icon-projects">
-                                <use xlinkHref={`${Icons}#icon-arrow-right`}></use>
-                            </svg>
-                        </Link>                        
+                    {/* name */}
+                    <div className="main__home section">
+                        <HeroBackground/>
+                        <h1 className="heading-primary"><span>Sumit</span> Joshi</h1>
                     </div>
 
-                    <div className="main__projects--project1">
-                        <div className="main__projects__background"></div>
-                        <div className="project-text">
-                            <h2>App</h2>
-                            <h1>Emaily</h1>
-                            <Link className="project-link" to="/projects/emaily">See project</Link>
+                    {/* about me  */}
+                    <div className="main__about section">
+                        <div className="bar-left"></div>
+    
+                        <p className="main__about--text">
+                            <span className="main__about--text--header red">Hi!</span>, I am
+                            <span className="green">&nbsp;Sumit Joshi</span>. I'm a
+                            <span className="red">&nbsp;front-end software developer&nbsp;</span>
+                            based in Delhi, India. I am comfortable throughout the stack, but my passion lies in front-end, at the intersection of code and design.
+                            <br/>
+                            Outside of code, I like to <span className="red">&nbsp;draw&nbsp;</span>, play 
+                            <span className="red">&nbsp;video games&nbsp;</span> and convert my ideas to video games.
+                        </p>
+    
+                    </div>
+    
+                    {/* skills section */}
+                    <div className="main__skills section">
+                        <div className="bar-left"></div>
+                        
+                        <h2 className="main__skills__heading">skills</h2>
+    
+                        <ul className="skills-list">
+                            <li className="skills-list__item skills-list__heading green">front</li>
+                            <li className="skills-list__item">html</li>
+                            <li className="skills-list__item">css</li>
+                            <li className="skills-list__item">sass</li>
+                            <li className="skills-list__item">javascript es6</li>
+                            <li className="skills-list__item">jQuery</li>
+                            <li className="skills-list__item">react.js</li>
+                            <li className="skills-list__item">redux</li>
+                            <li className="skills-list__item skills-list__heading red">back</li>
+                            <li className="skills-list__item">node.js</li>
+                            <li className="skills-list__item">express.js</li>
+                            <li className="skills-list__item">npm</li>
+                            <li className="skills-list__item">mongoDB</li>
+                            <li className="skills-list__item">mysql</li>
+                            <li className="skills-list__item">postresql</li>
+                            <li className="skills-list__item">rest</li>
+                            <li className="skills-list__item skills-list__heading green">tools</li>
+                            <li className="skills-list__item">git</li>
+                            <li className="skills-list__item">adobe XD</li>
+                            <li className="skills-list__item">adobe photoshop</li>
+                            <li className="skills-list__item">adobe illustrator</li>
+                            <li className="skills-list__item">visual code</li>
+                        </ul>
+                    </div>
+    
+                    {/* projects section */}
+                    <div className="main__projects section">
+                        <div className="bar-left"></div>
+                        
+                        <div className="main__projects__header">
+                            <h3 className="recents-heading">
+                                <span>Recent</span>
+                                <span>Projects</span>
+                            </h3>
+                            <Link to="/projects" className="main__projects__header--link">
+                                See all projects
+                                <svg className="icon-projects">
+                                    <use xlinkHref={`${Icons}#icon-arrow-right`}></use>
+                                </svg>
+                            </Link>                        
                         </div>
-
-                    </div>
-                    <div className="main__projects--project2">
-                        <div className="main__projects__background"></div>
-                        <div className="project-text">
-                            <h2>App</h2>
-                            <h1>Burger Builder</h1>
-                            <Link className="project-link" to="/projects/burger">See Project</Link>
+    
+                        <div className="main__projects--project1">
+                            <div className="main__projects__background"></div>
+                            <div className="project-text">
+                                <h2>App</h2>
+                                <h1>Emaily</h1>
+                                <Link className="project-link" to="/projects/emaily">See project</Link>
+                            </div>
+    
+                        </div>
+                        <div className="main__projects--project2">
+                            <div className="main__projects__background"></div>
+                            <div className="project-text">
+                                <h2>App</h2>
+                                <h1>Burger Builder</h1>
+                                <Link className="project-link" to="/projects/burger">See Project</Link>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
-    }
-}
+            );
+        }}    
+    />
+)
 
 export default Home;
--- src/containers/ProjectsList/ProjectsList.jsx
+++ src/containers/ProjectsList/ProjectsList.jsx
@@ -1,61 +1,47 @@
-import React, {Component} from 'react'
+import React from 'react'
 import NavBottom from '../../components/NavBottom/NavBottom';
-import {Link} from 'react-router-dom';
-import $ from 'jquery';
-import 'fullpage.js';
+import { Link } from 'react-router-dom';
+import ReactFullpage from '@fullpage/react-fullpage';
 import Icons from '../../assets/svg/sprite.svg';
+import $ from 'jquery'
 import './ProjectsList.css';
 import './ProjectListAnimate.css';
 
-class ProjectsList extends Component {
+const fullpageProjectsOptions = {
+    anchors: ['projects'],
+    callbacks: ['afterSlideLoad']
+}
 
-    componentDidMount() {
-        $(document)
-            .ready(function () {
-                $('#projects-all').fullpage({
-                    anchors: ['projects'],
-                    normalScrollElements: '.slide',
+const ProjectsList = () => (
+    <ReactFullpage {...fullpageProjectsOptions}
+        render={({ state, fullpageApi }) => {
 
-                    afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-                        $(".bottom-nav__list__item.active").removeClass("active");
-                        $(".bottom-nav__list__item")
-                            .eq(slideIndex)
-                            .addClass("active");
-                    }
-                });
+            if (state.callback === "afterSlideLoad") {
+                $(".bottom-nav__list__item.active").removeClass("active");
+                $(".bottom-nav__list__item")
+                    .eq(state.destination.index)
+                    .addClass("active");
+            }
 
-                $('.section').on('DOMMouseScroll mousewheel', function (e) {
-                    if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
-                        $.fn.fullpage.moveSlideRight();
-                    } else {
-                        $.fn.fullpage.moveSlideLeft();
-                    }
-                });
+            $('.section').on('DOMMouseScroll mousewheel', function (e) {
+                if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
+                    fullpageApi.moveSlideRight();
+                } else {
+                    fullpageApi.moveSlideLeft();
+                }
             });
-    }
-    shouldComponentUpdate(nextProps, nextState) {
 
-        return nextProps.location.pathname !== this.props.location.pathname
-    }
+            return (
+                <div className="projects-scroll">
+                    <Link to="/" className="icon-back">
+                        <svg className="icon-back-logo">
+                            <use xlinkHref={`${Icons}#icon-arrow-left`}></use>
+                        </svg>
+                    </Link>
 
-    componentWillUnmount() {
-        if (typeof $.fn.fullpage !== 'undefined') {
-            $.fn.fullpage.destroy('all');
-        }
-    }
-    render() {
-        return (
-            <div className="projects-scroll transition-item list-page">
-                <Link to="/" className="icon-back">
-                    <svg className="icon-back-logo">
-                        <use xlinkHref={`${Icons}#icon-arrow-left`}></use>
-                    </svg>
-                </Link>
-
-                <div className="backdrop"></div>
-                <NavBottom/>
-                <div id="projects-all">
-                    <div className="section">
+                    <div className="backdrop"></div>
+                    <NavBottom />
+                    <div className="section projects-all">
 
                         {/* first project */}
                         <div className="slide back-red fade">
@@ -64,21 +50,21 @@ class ProjectsList extends Component {
                                     className="project__thumbnail"
                                     id="p1-thumbnail"
                                     src={require("../../assets/images/emaily/emaily-project.jpg")}
-                                    alt="emaily project"/>
+                                    alt="emaily project" />
                                 <div className="project__info" id="p1-info">
                                     <h6>App</h6>
                                     <h4>Emaily</h4>
                                 </div>
                             </div>
                             <div className="project__link" id="p1-link">
-                                    <Link to="/projects/emaily">
-                                        See Project
+                                <Link to="/projects/emaily">
+                                    See Project
                                         <svg className="icon-link">
-                                            <use xlinkHref={`${Icons}#icon-arrow-right`}></use>
-                                        </svg>
-                                    </Link>
+                                        <use xlinkHref={`${Icons}#icon-arrow-right`}></use>
+                                    </svg>
+                                </Link>
                             </div>
-                            
+
                         </div>
 
                         {/* second projct */}
@@ -104,7 +90,7 @@ class ProjectsList extends Component {
                                 </Link>
                             </div>
                         </div>
-                        
+
                         {/* third project */}
                         <div className="slide back-yellow fade">
                             <div className="project">
@@ -152,12 +138,48 @@ class ProjectsList extends Component {
                                     </svg>
                                 </Link>
                             </div>
-                        </div>                      
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
-    }
-}
+            )
+        }} />
+)
+// 
+// componentDidMount() {
+//     $(document)
+//         .ready(function () {
+//             $('#projects-all').fullpage({
+//                 anchors: ['projects'],
+//                 normalScrollElements: '.slide',
+
+//                 afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+//                     $(".bottom-nav__list__item.active").removeClass("active");
+//                     $(".bottom-nav__list__item")
+//                         .eq(slideIndex)
+//                         .addClass("active");
+//                 }
+//             });
+
+//             $('.section').on('DOMMouseScroll mousewheel', function (e) {
+//                 if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
+//                     $.fn.fullpage.moveSlideRight();
+//                 } else {
+//                     $.fn.fullpage.moveSlideLeft();
+//                 }
+//             });
+//         });
+// }
+
+
+// shouldComponentUpdate(nextProps, nextState) {
+//     return nextProps.location.pathname !== this.props.location.pathname
+// }
+
+// componentWillUnmount() {
+//     if (typeof $.fn.fullpage !== 'undefined') {
+//         $.fn.fullpage.destroy('all');
+//     }
+// }
+
 
 export default ProjectsList;
