71a36ff83fc2d8497604fa32bc676374232806e3
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-11 15:33:00 +0530

    about me content added

--- src/containers/Portfolio/Home/Home.jsx
+++ src/containers/Portfolio/Home/Home.jsx
@@ -30,8 +30,10 @@ const Home = fullpageProps => (
                             <span className="red">&nbsp;front-end software developer&nbsp;</span>
                             based in Delhi, India. I am comfortable throughout the stack, but my passion lies in front-end, at the intersection of code and design.
                             <br/>
-                            Outside of code, I like to <span className="red">&nbsp;draw&nbsp;</span>, play 
-                            <span className="red">&nbsp;video games&nbsp;</span> and convert my ideas to video games.
+                            Being a <span className="red">self-taught developer</span>, I have a passion for code and an innate drive to learn.
+                            <br/>
+                            Outside of code, I like to <span className="red">&nbsp;draw</span>, <span className="red">&nbsp;sculpt</span>, play 
+                            <span className="red">&nbsp;video games</span> and convert my ideas into fun video games.
                         </p>
     
                     </div>
