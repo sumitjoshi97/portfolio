e3d8ffe88826040dae3fb2dd9cea106db6c7158a
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-11 15:30:53 +0530

    loader component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Loading.css'
+
+const Loading = () => (
+    <div className="loading">
+        <div className="lds-ripple"><div></div><div></div></div>
+    </div>
+)
+
+export default Loading
