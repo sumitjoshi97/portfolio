f631eba76e09f5b04b6b4bfd3d079ef1bcfaf8a1
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-12 17:31:13 +0530

    project cart added

---
+++ src/containers/ProjectsList/ProjectCart/ProjectCart.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Project from '../Project/Project';
+
+const ProjectCart = () => (
+    <Project 
+        projectType="App"
+        projectName="ShopCart"
+        projectInfo="A shopping web-app in which user can add t-shirts to cart, filter products by size, and
+                    at last purchase their order and check their previous orders."
+        headerImage={require('../../../assets/images/cart/cart.svg')}
+        headerStyle={['#341f97', '#5f27cd']}
+        theme={'#5f27cd'}
+        stacks = {['React js', 'Redux', 'SASS', 'Firebase']}
+        backImage={require('../../../assets/images/emaily/emaily-full.jpg')}
+        linkLanuchProject="https://bit.ly/_Emaily"
+        screenshots = {[ `${require('../../../assets/images/cart/home.jpg')}`,
+            `${require('../../../assets/images/cart/filters.jpg')}`,
+            `${require('../../../assets/images/cart/cartSummary.jpg')}`,
+            `${require('../../../assets/images/cart/signup.jpg')}`,
+            `${require('../../../assets/images/cart/checkout.jpg')}`,
+            `${require('../../../assets/images/cart/checkout_form.jpg')}`,
+            `${require('../../../assets/images/cart/orders.jpg')}` ]}
+        projectPrev='natours'
+        projectNext='emaily'
+        />
+);
+
+export default ProjectCart
