df162694a1475cd4ee90f3306b7fce78451696d0
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-05 18:40:11 +0530

    routes modified

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import Loading from './components/Loading/Loading'
 const ProjectsList = Loadable({ loader: () => import('./containers/ProjectsList/ProjectsList'), loading: Loading })
 const ProjectCart = Loadable({ loader: () => import('./containers/ProjectsList/ProjectCart/ProjectCart'), loading: Loading })
 const ProjectEmaily = Loadable({ loader: () => import('./containers/ProjectsList/ProjectEmaily/ProjectEmaily'), loading: Loading })
-const ProjectNatours = Loadable({ loader: () => import('./containers/ProjectsList/ProjectNatours/ProjectNatours'), loading: Loading })
 const ProjectSmartBrain = Loadable({ loader: () => import('./containers/ProjectsList/ProjectSmartBrain/ProjectSmartBrain'), loading: Loading })
 const ProjectBurger = Loadable({ loader: () => import('./containers/ProjectsList/ProjectBurger/ProjectBurger'), loading: Loading })
+const ProjectDevConnect = Loadable({ loader: () => import('./containers/ProjectsList/ProjectDevConnect/ProjectDevConnect'), loading: Loading })
 
 // main App Component
 const App = () => {
@@ -22,8 +22,8 @@ const App = () => {
                     <Route exact path="/projects" component={ProjectsList} />
                     <Route exact path="/projects/cart" component={ProjectCart} />
                     <Route exact path="/projects/emaily" component={ProjectEmaily} />
-                    <Route exact path="/projects/natours" component={ProjectNatours} />
                     <Route exact path="/projects/smart-brain" component={ProjectSmartBrain} />
+                    <Route exact path="/projects/dev-connect" component={ProjectDevConnect} />
                     <Route path='/projects/burger' component={ProjectBurger} />
                     <Route path="/" component={Portfolio} />
                 </Switch>
