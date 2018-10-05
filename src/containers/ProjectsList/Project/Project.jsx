e949e8f50acaad2ac8f2661efc379878d1585aa9
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-05 18:39:23 +0530

    screenshots to show text and image

--- src/containers/ProjectsList/Project/Project.jsx
+++ src/containers/ProjectsList/Project/Project.jsx
@@ -1,122 +1,133 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import Icons from '../../../assets/svg/sprite.svg';
-import Header from './Header.jsx';
-import Footer from './Footer';
-import Radium from 'radium';
-import './Project.css';
-import ReallySmoothScroll from 'really-smooth-scroll';
-ReallySmoothScroll.shim();
+import { Link } from 'react-router-dom'
+import Icons from '../../../assets/svg/sprite.svg'
+import Header from './Header.jsx'
+import Footer from './Footer'
+import Radium from 'radium'
+import './Project.css'
+import ReallySmoothScroll from 'really-smooth-scroll'
+ReallySmoothScroll.shim()
 
-const Project = (props) => {
+const Project = props => {
+  // list of stack used in project
+  const stackList = props.stacks.map((stack, index) => {
+    return <li key={index}>{stack}</li>
+  })
 
-    // list of stack used in project
-    const stackList = props.stacks.map((stack, index) => {
-        return <li key={index}>{stack}</li>
-    })
+  // screenshots list of project
+  const shotsList = props.screenshots.map((shot, index) => (
+    <div className="project-full__summary-image" key={index}>
+      <div className="project-full__summary-image__shot">
+        <img
+          src={shot.image}
+          alt="summary screenshot"
+          className="project-full__summary-image__image"
+        />
+        <p className="project-full__summary-image__text">{shot.value}</p>
+      </div>
+    </div>
+  ))
 
-    // screenshots list of project
-    const shotsList = props.screenshots.map((shot, index) => (
-        <div className="project-full__summary-image" key={index}>
-            <img
-                src={shot}
-                alt="summary screenshot"
-                className="project-full__summary-image__image" />
-        </div>
-    ))
+  const backgroundStyle = {
+    backgroundImage: `linear-gradient(to right, rgba(255,255,255, 0.6) 0%,  rgba(255,255,255, 0.6) 100%), url(${
+      props.backImage
+    })`
+  }
 
-    const backgroundStyle = {
-        backgroundImage: `linear-gradient(to right, rgba(255,255,255, 0.6) 0%,  rgba(255,255,255, 0.6) 100%), url(${props.backImage})`
+  const theme = {
+    backgroundColor: `${props.theme}`
+  }
+
+  const linkStyle = {
+    ':hover': {
+      color: '#fff',
+      backgroundColor: `${props.theme}`,
+      border: `1.5px solid ${props.theme}`
     }
+  }
 
-    const theme = {
-        backgroundColor: `${props.theme}`
-    }
+  return (
+    <div className="project-full">
+      <Link to="/projects" className="icon-back">
+        <svg
+          className="icon-back-logo"
+          style={{
+            fill: 'black'
+          }}>
+          <use xlinkHref={`${Icons}#icon-arrow-left`} />
+        </svg>
+      </Link>
+      <Header
+        projectType={props.projectType}
+        projectName={props.projectName}
+        projectInfo={props.projectInfo}
+        headerImage={props.headerImage}
+        headerStyle={props.headerStyle}
+        theme={props.theme}
+      />
 
-    const linkStyle = {
-        ':hover': {
-            color: '#fff',
-            backgroundColor: `${props.theme}`,
-            border: `1.5px solid ${props.theme}`
-        }
-    }
+      <div className="project-full__stack" style={theme}>
+        <h3>Stack</h3>
+        <ul className="project-full__stack--list">{stackList}</ul>
+      </div>
 
-    return (
-        <div className="project-full">
-            <Link to="/projects" className="icon-back">
-                <svg
-                    className="icon-back-logo"
-                    style={{
-                        fill: 'black'
-                    }}>
-                    <use xlinkHref={`${Icons}#icon-arrow-left`}></use>
-                </svg>
-            </Link>
-            <Header projectType={props.projectType}
-                projectName={props.projectName}
-                projectInfo={props.projectInfo}
-                headerImage={props.headerImage}
-                headerStyle={props.headerStyle}
-                theme={props.theme}
-            />
+      {/* screenshot  */}
+      <div className="project-full__screenshot" style={backgroundStyle} />
 
-            <div className="project-full__stack" style={theme}>
-                <h3>Stack</h3>
-                <ul className="project-full__stack--list">
-                    {stackList}
-                </ul>
-            </div>
+      {/* project launch button */}
+      <div className="project-full__launch">
+        <a
+          href={props.linkLanuchProject}
+          target="blank"
+          className="project-full__launch--link"
+          style={linkStyle}>
+          Launch Project
+        </a>
+      </div>
 
-            {/* screenshot  */}
-            <div className="project-full__screenshot" style={backgroundStyle} />
+      {/* summary shots */}
+      <div>{shotsList}</div>
 
-            {/* project launch button */}
-            <div className="project-full__launch">
-                <a
-                    href={props.linkLanuchProject}
-                    target="blank"
-                    className="project-full__launch--link"
-                    style={linkStyle}>
-                    Launch Project
-                </a>
-            </div>
+      {/* navigation */}
+      <div className="project-full__navigation" style={theme}>
+        {/* prev button */}
+        <Link
+          to={`/projects/${props.projectPrev}`}
+          className="project-full__navigation__section"
+          style={{ left: '4%' }}>
+          <svg className="icon-logo icon-logo--prev">
+            <use xlinkHref={`${Icons}#icon-arrow-left`} />
+          </svg>
+          <span
+            style={{
+              left: '6rem'
+            }}>
+            Prev
+          </span>
+        </Link>
+        <div className="center">Projects</div>
 
-            {/* summary shots */}
-            <div>
-                {shotsList}
-            </div>
+        {/* next project button */}
+        <Link
+          to={`/projects/${props.projectNext}`}
+          className="project-full__navigation__section"
+          style={{ right: '4%' }}>
+          <svg className="icon-logo icon-logo--next">
+            <use xlinkHref={`${Icons}#icon-arrow-right`} />
+          </svg>
+          <span
+            style={{
+              right: '6rem'
+            }}>
+            Next
+          </span>
+        </Link>
+      </div>
 
-            {/* navigation */}
-            <div className="project-full__navigation" style={theme}>
-            
-                {/* prev button */}
-                <Link to={`/projects/${props.projectPrev}`} className="project-full__navigation__section" style={{ left: '4%' }}>
-                    <svg className="icon-logo icon-logo--prev">
-                        <use xlinkHref={`${Icons}#icon-arrow-left`}></use>
-                    </svg>
-                    <span
-                        style={{
-                            left: '6rem'
-                        }}>Prev</span>
-                </Link>
-                <div className="center">Projects</div>
-
-                {/* next project button */}
-                <Link to={`/projects/${props.projectNext}`} className="project-full__navigation__section" style={{ right: '4%' }}>
-                    <svg className="icon-logo icon-logo--next">
-                        <use xlinkHref={`${Icons}#icon-arrow-right`}></use>
-                    </svg>
-                    <span
-                        style={{
-                            right: '6rem'
-                        }}>Next</span>
-                </Link>
-            </div>
-            
-            {/* footer */}
-            <Footer theme={props.theme} />
-        </div>
-    )
+      {/* footer */}
+      <Footer theme={props.theme} />
+    </div>
+  )
 }
 
-export default Radium(Project);
+export default Radium(Project)
