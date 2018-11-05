e8ad86ea7fb06208e4d29aa660d674eb89428bb3
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-11-05 17:23:47 +0530

    Add background lines

--- src/containers/Portfolio/PortFolio.jsx
+++ src/containers/Portfolio/PortFolio.jsx
@@ -1,48 +1,60 @@
 import React, { Component } from 'react'
-import LeftNav from './LeftNav/LeftNav';
-import RightNav from './RightNav/RightNav';
-import Home from './Home/Home';
-import NavToggle from './NavToggle/NavToggle';
-import Navigation from './Navigation/Navigation';
-import './PortFolio.css';
+import LeftNav from './LeftNav/LeftNav'
+import RightNav from './RightNav/RightNav'
+import Home from './Home/Home'
+import NavToggle from './NavToggle/NavToggle'
+import Navigation from './Navigation/Navigation'
+import './PortFolio.css'
 
 const fullpageOptions = {
-    anchors: ["home", "about", "skills", "recents"],
-    menu: "#fixed-nav"
+  anchors: ['home', 'about', 'skills', 'recents'],
+  menu: '#fixed-nav'
 }
 
 class Portfolio extends Component {
-    state = {
-        isNavActive: false
-    }
+  state = {
+    isNavActive: false
+  }
 
-    handleToggle = () => {
-        this.setState({
-            isNavActive: !this.state.isNavActive
-        });
-    }
+  handleToggle = () => {
+    this.setState({
+      isNavActive: !this.state.isNavActive
+    })
+  }
 
-    renderNav = () => {
-        if (this.state.isNavActive) {
-            return <Navigation toggle={this.handleToggle} isNavActive={this.state.isNavActive} />
-        }
-        return (
-            <div className="portfolio">
-                <LeftNav />
-                <Home {...fullpageOptions} />
-                <RightNav />
-            </div>
-        )
+  renderNav = () => {
+    if (this.state.isNavActive) {
+      return (
+        <Navigation
+          toggle={this.handleToggle}
+          isNavActive={this.state.isNavActive}
+        />
+      )
     }
+    return (
+      <div className="portfolio">
+        <div className="box">
+          <div className="bar" />
+          <div className="bar" />
+          <div className="bar" />
+          <div className="bar" />
+          <div className="bar" />
+        </div>
+        <LeftNav />
+        <Home {...fullpageOptions} />
+        <RightNav />
+      </div>
+    )
+  }
 
-    render() {
-        return (
-            <div className="home ">
-                <NavToggle toggle={this.handleToggle} />
-                {this.renderNav()}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="home ">
+        <NavToggle toggle={this.handleToggle} />
+        {this.renderNav()}
+      </div>
+    )
+  }
 }
 
-export default Portfolio;
+export default Portfolio
