75c658452f4083bff4c5d3323b85a436b4c20807
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-11-09 00:10:02 +0530

    Change markup for header section in main page

--- src/containers/Portfolio/Home/Header/index.jsx
+++ src/containers/Portfolio/Home/Header/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
 export default () => (
-  <div className="main__home section">
+  <div className="main__header section">
     <div className="background-name">
       crea
       <span>tive</span>
     </div>
-    <h1 className="heading-primary">
-      <span>Sumit</span> Joshi
-    </h1>
+    <div className="main__header__text">
+      <h1 className="main__header__text__primary">
+        <span>Sumit</span> Joshi
+      </h1>
+      <div className="underline-grp">
+        <div className="underline underline--white" />
+        <div className="underline underline--grey" />
+      </div>
+
+      <h2 className="main__header__text__secondary">PORTFOLIO</h2>
+    </div>
   </div>
 )
