ec3a0185939ddde75201c5e8234131117d727636
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-11-09 23:28:20 +0530

    refactored

--- src/containers/Portfolio/Home/Header/index.jsx
+++ src/containers/Portfolio/Home/Header/index.jsx
@@ -8,14 +8,14 @@ export default () => (
     </div>
     <div className="main__header__text">
       <h1 className="main__header__text__primary">
-        <span>Sumit</span> Joshi
+        Sumit Joshi
       </h1>
       <div className="underline-grp">
         <div className="underline underline--white" />
         <div className="underline underline--grey" />
       </div>
 
-      <h2 className="main__header__text__secondary">PORTFOLIO</h2>
+      <h2 className="main__header__text__secondary">portfolio</h2>
     </div>
   </div>
 )
