c2b63e5a1073911eb61a48e5162ef2a08f1f9fee
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-11-10 23:34:18 +0530

    Update about section markup

--- src/containers/Portfolio/Home/About/index.jsx
+++ src/containers/Portfolio/Home/About/index.jsx
@@ -10,15 +10,17 @@ export default () => (
       <span className="main__about__text__header red">Hi!</span>, I am
       <span className="green">&nbsp;Sumit Joshi</span>, a
       <span className="red">&nbsp;front-end software developer&nbsp;</span>
-      based in Delhi, India. I am comfortable throughout the stack, but my
-      passion lies in front-end, at the intersection of code and design.
+      based in Delhi, India. Driven by passion and curiosity, I try to learn and
+      discover new things everyday.
       <br />
-      Being a <span className="red">self-taught developer</span>, I have a
-      passion for code and an innate drive to learn.
+      I have worked in back-end tech but I am fond of front-end development -
+      where design and code intersects.
       <br />
-      Outside of code, I like to <span className="red">&nbsp;draw</span>, play
-      <span className="red">&nbsp;video games</span> and convert my ideas into
-      fun video games.
+      Outside of code, you can see me in some{' '}
+      <span className="red">meetup</span>, watching{' '}
+      <span className="red">anime</span>,{' '}
+      <span className="red">drawing</span> and playing{' '}
+      <span className="red">video games</span>
     </p>
   </div>
 )
