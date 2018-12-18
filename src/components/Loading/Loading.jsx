b6678aba037957eb48121c09327516d437bf5350
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-12-18 13:38:26 +0530

    Refactor markup

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import './Loading.css'
 
 const Loading = () => (
-    <div className="loading">
-        <div className="lds-ripple"><div></div><div></div></div>
+  <div className="loading">
+    <div className="lds-ripple">
+      <div />
+      <div />
     </div>
+  </div>
 )
 
 export default Loading
