98fe5ce08024ec5aa87d908a10d6563dc9e42b85
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-12-19 23:25:11 +0530

    Replace css import with scss

--- src/containers/Portfolio/NavToggle/NavToggle.jsx
+++ src/containers/Portfolio/NavToggle/NavToggle.jsx
@@ -1,16 +1,16 @@
-import React, {Component} from 'react'
-import './NavToggle.css';
+import React, { Component } from 'react'
+import './NavToggle.scss';
 
 class NavToggle extends Component {
-    render() {
-        return (
-            <div className="navigation">
-                <div className="nav-toggle" onClick={this.props.toggle}>
-                    <span></span>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="navigation">
+        <div className="nav-toggle" onClick={this.props.toggle}>
+          <span></span>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default NavToggle;
