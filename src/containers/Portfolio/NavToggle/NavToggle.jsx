288379c63dad062389aa23e56b704efd189fb226
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-12-30 22:45:01 +0530

    Prettify code

--- src/containers/Portfolio/NavToggle/NavToggle.jsx
+++ src/containers/Portfolio/NavToggle/NavToggle.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
-import './NavToggle.scss';
+import './NavToggle.scss'
 
 class NavToggle extends Component {
   render() {
     return (
       <div className="navigation">
         <div className="nav-toggle" onClick={this.props.toggle}>
-          <span></span>
+          <span />
         </div>
       </div>
     )
   }
 }
 
-export default NavToggle;
+export default NavToggle
