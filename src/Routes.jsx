dda812087174f4edb911c67239586a366ed72af3
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-01-02 15:38:16 +0530

    Add NotFound page route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,40 +3,51 @@ import { lazy } from 'react'
 export default [
   {
     component: lazy(() => import('./containers/ProjectsList/ProjectsList')),
-    path: '/projects'
+    path: '/projects',
+    exact: true
   },
   {
     component: lazy(() =>
       import('./containers/ProjectsList/ProjectCart/ProjectCart')
     ),
-    path: '/projects/cart'
+    path: '/projects/cart',
+    exact: true
   },
   {
     component: lazy(() =>
       import('./containers/ProjectsList/ProjectEmaily/ProjectEmaily')
     ),
-    path: '/projects/emaily'
+    path: '/projects/emaily',
+    exact: true
   },
   {
     component: lazy(() =>
       import('./containers/ProjectsList/ProjectSmartBrain/ProjectSmartBrain')
     ),
-    path: '/projects/smart-brain'
+    path: '/projects/smart-brain',
+    exact: true
   },
   {
     component: lazy(() =>
       import('./containers/ProjectsList/ProjectDevConnect/ProjectDevConnect')
     ),
-    path: '/projects/dev-connect'
+    path: '/projects/dev-connect',
+    exact: true
   },
   {
     component: lazy(() =>
       import('./containers/ProjectsList/ProjectReactChat/ProjectReactChat')
     ),
-    path: '/projects/react-chat'
+    path: '/projects/react-chat',
+    exact: true
   },
   {
     component: lazy(() => import('./containers/Portfolio/PortFolio')),
-    path: '/'
+    path: '/',
+    exact: true
+  },
+  {
+    component: lazy(() => import('./containers/NotFound/NotFound')),
+    path: ''
   }
 ]
