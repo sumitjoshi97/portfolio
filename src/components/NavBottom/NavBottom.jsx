0d215377627f297395a30dc85ec2a61d2895b8f9
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-22 22:44:51 +0530

    chore: add link for movie-rolls project

--- src/components/NavBottom/NavBottom.jsx
+++ src/components/NavBottom/NavBottom.jsx
@@ -2,31 +2,36 @@ import React from 'react'
 import './NavBottom.scss'
 
 const NavBottom = () => (
-  <nav className="bottom-nav">
-    <ul className="bottom-nav__list">
-      <li className="bottom-nav__list__item active">
-        <a className="bottom-nav__list__item__link" href="#projects/">
+  <nav className='bottom-nav'>
+    <ul className='bottom-nav__list'>
+      <li className='bottom-nav__list__item active'>
+        <a className='bottom-nav__list__item__link' href='#projects/'>
           01
         </a>
       </li>
-      <li className="bottom-nav__list__item">
-        <a className="bottom-nav__list__item__link" href="#projects/1">
+      <li className='bottom-nav__list__item'>
+        <a className='bottom-nav__list__item__link' href='#projects/1'>
           02
         </a>
       </li>
-      <li className="bottom-nav__list__item">
-        <a className="bottom-nav__list__item__link" href="#projects/2">
+      <li className='bottom-nav__list__item'>
+        <a className='bottom-nav__list__item__link' href='#projects/2'>
           03
         </a>
       </li>
-      <li className="bottom-nav__list__item">
-        <a className="bottom-nav__list__item__link" href="#projects/3">
+      <li className='bottom-nav__list__item'>
+        <a className='bottom-nav__list__item__link' href='#projects/3'>
           04
         </a>
       </li>
-      <li className="bottom-nav__list__item">
-        <a className="bottom-nav__list__item__link" href="#projects/4">
+      <li className='bottom-nav__list__item'>
+        <a className='bottom-nav__list__item__link' href='#projects/4'>
           05
+        </a>
+      </li>
+      <li className='bottom-nav__list__item'>
+        <a className='bottom-nav__list__item__link' href='#projects/5'>
+          06
         </a>
       </li>
     </ul>
