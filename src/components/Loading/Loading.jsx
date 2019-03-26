8312cb4ef0480c1dd5fb5e715fac8c9118f173b3
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-03-26 14:25:25 +0530

    chore: update loading component

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import './Loading.css'
 
 const Loading = () => (
-  <div className="loading">
-    <div className="lds-ripple">
-      <div />
-      <div />
+  <div className='loading'>
+    <div className='container'>
+      <div className='loading-header'>Loading</div>
+      <div className='lds-ripple'>
+        <div />
+        <div />
+      </div>
     </div>
   </div>
 )
