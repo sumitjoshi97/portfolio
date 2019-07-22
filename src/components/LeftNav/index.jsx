2623fa612ac647c02e6534f2efb62d5a6c6f2dbe
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-07-22 13:56:50 +0530

    chore: add left nav

---
+++ src/components/LeftNav/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Social from '../Social'
+import './styles.scss'
+export default function LeftNav() {
+  return (
+    <div className="left-nav">
+      <Social />
+    </div>
+  )
+}
