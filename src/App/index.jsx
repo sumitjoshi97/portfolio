e4bb129e1484ad95c8b151b1fd0914f95f3e7299
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-07-25 21:54:34 +0530

    chore: move home and projects route to bottom

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import NavBar from '../components/NavBar'
 import Home from '../views/Home'
@@ -9,35 +9,37 @@ import Loading from '../components/Loading'
 import routes from './Routes'
 import LeftNav from '../components/LeftNav'
 
-// main App Component
-const App = () => (
-  <Router>
-    <ScrollToTop>
-      <Suspense fallback={<Loading />}>
-        <Switch>
-          <>
-            <NavBar />
-            <LeftNav />
-            <Route path="/projects" component={Projects} />
-            <Route exact path="/" component={Home} />
-          </>
-          {routes.map(route => (
-            <Route
-              key={route.path}
-              path={route.path}
-              exact={route.exact}
-              component={route.component}
-              // render={() => (
-              //   <Suspense fallback={<Loading />}>
-              //     <route.component />
-              //   </Suspense>
-              // )}
-            />
-          ))}
-        </Switch>
-      </Suspense>
-    </ScrollToTop>
-  </Router>
-)
-
-export default App
+export default function App() {
+  return (
+    <>
+      <div
+        style={{
+          background: '#1f2335',
+          position: 'fixed',
+          height: '100%',
+          width: '100%',
+        }}
+      />
+      <ScrollToTop>
+        <Suspense fallback={<Loading />}>
+          <Switch>
+            {routes.map(route => (
+              <Route
+                key={route.path}
+                path={route.path}
+                exact={route.exact}
+                component={route.component}
+              />
+            ))}
+            <>
+              <NavBar />
+              <LeftNav />
+              <Route exact path="/projects" component={Projects} />
+              <Route exact path="/" component={Home} />
+            </>
+          </Switch>
+        </Suspense>
+      </ScrollToTop>
+    </>
+  )
+}
