5153b988652d7caf6315e3caada5645fd7fe737d
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-07-26 18:12:34 +0530

    chore: update filename

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import './Loading.css'
+import './styles.css'
 
 const Loading = () => (
-  <div className='loading'>
-    <div className='container'>
-      <div className='loading-header'>Loading</div>
-      <div className='lds-ripple'>
-        <div />
-        <div />
-      </div>
-    </div>
+  <div className="loading">
+    <div className="loading-header">Loading</div>
+    <div className="loader">Loading...</div>
   </div>
 )
 
