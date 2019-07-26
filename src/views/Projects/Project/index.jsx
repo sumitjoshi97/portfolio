1f00a52397d2a41dfae303fffb0920e0dc1b7867
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-07-26 18:24:10 +0530

    chore: update elements positioning

--- src/views/Projects/Project/index.jsx
+++ src/views/Projects/Project/index.jsx
@@ -3,49 +3,31 @@ import { Link } from 'react-router-dom'
 
 import './styles.scss'
 
-const ProjectsListItem = ({ id, imgUrl, title, link, theme }) => {
+const Project = ({ id, imgUrl, title, link, theme }) => {
   return (
-    <div className='slide' style={{ backgroundColor: theme }}>
-      <div className='project-box'>
-        <div className='bar' />
-        <div className='bar' />
-        <div className='bar' />
-        <div className='bar' />
-        <div className='bar' />
+    <div className="project">
+      <div className="project__thumbnail" id={`p${id}-thumbnail`}>
+        <img className="project__thumbnail__image" src={imgUrl} alt={title} />
       </div>
 
-      <div className='project-list-item'>
-        <img
-          className='project-list-item__thumbnail'
-          id={`p${id}-thumbnail`}
-          src={imgUrl}
-          alt={title}
-        />
-
-        <div className='project-list-item__info' id={`p${id}-info`}>
-          <div className='project-list-item__header'>
-            <h3
-              className='project-list-item__info__header--primary'
-              style={{ color: theme }}
-            >
-              App
-            </h3>
-            <h2 className='project-list-item__info__header--secondary'>
-              {title}
-            </h2>
-          </div>
-
-          <Link
-            to={`projects/${link}`}
-            className='project-list-item__info__link'
+      <div className="project__info" id={`p${id}-info`}>
+        <div className="project__header">
+          <h3
+            className="project__info__header--primary"
+            style={{ color: theme }}
           >
-            view project
-            <div className='project-list-item__info__link__bottom' />
-          </Link>
+            App
+          </h3>
+          <h2 className="project__info__header--secondary">{title}</h2>
         </div>
+
+        <Link to={`/projects/${link}`} className="project__info__link">
+          view project
+          <div className="project__info__link__bottom" />
+        </Link>
       </div>
     </div>
   )
 }
 
-export default ProjectsListItem
+export default Project
