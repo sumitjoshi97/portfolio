a43e777b08935ed2957a602470a3073ec39d18e1
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-08-19 23:34:31 +0530

    chore: add resume link

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -6,6 +6,15 @@ export default function NavBar() {
   return (
     <ul className="nav">
       <li>
+        <a
+          href="https://drive.google.com/file/d/1QkUAVM7ZRFi-nwQb_B3rzB-q6uHn1_4d/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Resume
+        </a>
+      </li>
+      <li>
         <NavLink to="/">About</NavLink>
       </li>
       <li>
